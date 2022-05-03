63df513e3bcbd330364a5fc99aa949ffcdf840f2
zzzIDKu <619179378@qq.com>
2022-05-03 11:42:05 +0800

    11

--- datalab-69/datalab.c
+++ datalab-69/datalab.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+#include <stdlib.h>
+
+/*
+获取x的绝对值
+示例: absVal(-1) = 1
+说明：-TMax <= x <= TMAX
+限制操作: ! ~ & ^ | + << >>
+操作数量: 10
+难度: 4
+*/
+long absVal(long x){
+    return ((x>>63)&(~x+1))|(((~(x>>63))& (x)));
+}
+int main(){
+    for(long x=0;x!=LONG_MAX;x++)
+        if(x!=absVal(x))
+            printf("wrong\n");
+
+
+}
 
