9d30a60e318eec1254ff010a6ac9556e63ec1c2c
zzzIDKu <619179378@qq.com>
2022-05-04 13:04:39 +0800

    69_2 done

--- datalab-69/datalab.c
+++ datalab-69/datalab.c
@@ -14,6 +14,17 @@
 long absVal(long x){
     return ((x>>63)&(~x+1))|(((~(x>>63))& (x)));
 }
+/*
+
+- 判断x+y是否溢出，溢出返回0，反之
+- 示例：addOK(0x8000000,0x8000000) = 0
+- 限制操作：! ~ & ^ | + << >>
+- 操作数量：20
+- 难度：3
+*/
+long addOK(long x,long y){
+    return (((x^y)>>63)|(~(x^(x+y)>>63)))&1;
+}
 int main(){
     for(long x=0;x!=LONG_MAX;x++)
         if(x!=absVal(x))
